1ae52827869ecde377501bd4dd14f2f78b3754d2
priyankasharma042 <vs6080364@gmail.com>
2026-09-12 11:04:33 +0530

    Add files via upload

---
+++ table.py
@@ -0,0 +1,9 @@
+num= int(input("enter a number:(2 to 10):"))
+if num >= 2 and num <= 10:
+        for i in range(1,11):
+            print(num,"x",i,"=",num*i)
+        else:
+            print("please enter a number 2 and 10.")
+
+
+
